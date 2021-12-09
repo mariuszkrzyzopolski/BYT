0e543958e971967da064ff11b030d3f4156eda9d
warhir <krzyzopolski.mariusz@gmail.com>
2021-12-09 16:13:39 +0100

    plant model and delete btnn

--- models.py
+++ models.py
@@ -11,3 +11,15 @@ class User(db.Model):
         self.username = username
         self.password = password
         self.email = email
+
+
+class Plant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(500))
+    photo = db.Column(db.String(10))
+    ownership = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, description, photo, ownership):
+        self.description = description
+        self.photo = photo
+        self.ownership = ownership

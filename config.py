71d1b2ef289bc4de101467fc570ab4a1c894de6c
warhir <krzyzopolski.mariusz@gmail.com>
2022-01-02 13:01:42 +0100

    send default email every 5 min

--- config.py
+++ config.py
@@ -4,3 +4,10 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://sql11454497:RwkSDDxD8v@sql11.freemysqlhosting.net:3306' \
                               '/sql11454497 '
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER: 'localhost'
+    MAIL_SERVER = 'smtp.mailtrap.io'
+    MAIL_PORT = 2525
+    MAIL_USERNAME = '59193a4ea165c8'
+    MAIL_PASSWORD = '6de17a5d4d0776'
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
--- send_alert.py
+++ send_alert.py
@@ -1,22 +1,23 @@
 import atexit
-import sys
 
 from flask_mail import Mail, Message
 from apscheduler.schedulers.background import BackgroundScheduler
 
 
-def send_mail(app):
-    mail = Mail(app)
-    msg = Message("Sample",
-                  sender="warhir@gmail.com",
-                  recipients=["m92686260@gmail.com"])
-    print(app.config['MAIL_USERNAME'], file=sys.stderr)
-    mail.send(msg)
+def send_mail():
+    with app.app_context():
+        mail = Mail(app)
+        msg = Message("Sample",
+                      sender="mucholowka@mail.com",
+                      recipients=["m92686260@gmail.com"])
+        mail.send(msg)
 
 
-def schedule_mail(app):
+def schedule_mail(mail_app):
+    global app
+    app = mail_app
     scheduler = BackgroundScheduler()
-    scheduler.add_job(func=send_mail(app), trigger="interval", minutes=5)
+    scheduler.add_job(func=send_mail, trigger="interval", minutes=5)
     scheduler.start()
 
     # Shut down the scheduler when exiting the app

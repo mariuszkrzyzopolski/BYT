262e2ccc7050941a16e9bca711c787cca60e2d46
warhir <krzyzopolski.mariusz@gmail.com>
2022-01-05 21:19:54 +0100

    add alert

--- controllers/PlantController.py
+++ controllers/PlantController.py
@@ -23,11 +23,27 @@ def send_mail(message, subject, recipient):
         mail.send(msg)
 
 
-def schedule_mail(mail_app, context):
+def choose_alert_kind(form):
+    if form == "Podlewanie":
+        return "Podlej roślinę"
+    if form == "Nawożenie":
+        return "Nawieź roślinę"
+
+
+def schedule_mail(mail_app, context, frequency):
     global app
     app = mail_app
     scheduler = BackgroundScheduler()
-    scheduler.add_job(func=send_mail, kwargs=context, trigger="interval", minutes=5)
+    if frequency == "Codziennie":
+        scheduler.add_job(func=send_mail, kwargs=context, trigger="interval", days=1)
+    if frequency == "Tygodniowo":
+        scheduler.add_job(func=send_mail, kwargs=context, trigger="interval", days=7)
+    if frequency == "Miesięcznie":
+        scheduler.add_job(func=send_mail, kwargs=context, trigger="interval", months=1)
+    if frequency == "Kwartalnie":
+        scheduler.add_job(func=send_mail, kwargs=context, trigger="interval", months=3)
+    if frequency == "Rocznie":
+        scheduler.add_job(func=send_mail, kwargs=context, trigger="interval", years=1)
     scheduler.start()
 
     # Shut down the scheduler when exiting the app
--- routes/PlantRouting.py
+++ routes/PlantRouting.py
@@ -85,9 +85,11 @@ def remove(plant_id):
 def alert():
     if session.get("username") is not None:
         if request.method == 'GET':
-            return render_template('alerts.html')
+            return render_template('addNotification.html')
         else:
             email = User.query.filter_by(username=session.get('username')).first().email
-            context = {"recipient": email, "subject": "Test", "message": "Podlej tą roślinę"}
-            schedule_mail(app, context)
+            context = {"recipient": email, "subject": "Przypomnienie z aplikacji Muchołówka",
+                       "message": session.get("username") + " " + choose_alert_kind(request.form['kind']) + " " +
+                                  request.form['plant']}
+            schedule_mail(app, context, request.form['frequency'])
             return redirect(url_for("start"))

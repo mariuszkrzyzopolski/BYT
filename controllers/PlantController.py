f804b4a725d3fb79a23e220c4fe5484b671f9fa6
mariuszkrzyzopolski <39222705+mariuszkrzyzopolski@users.noreply.github.com>
2022-01-06 22:41:46 +0100

    Alerts (#7)
    * mail sender configuration
    
    * send default email every 5 min
    
    * rebase with session mail
    
    * get and post for alert
    
    * add alert
    
    * fix photo and list of plants
    
    * pytest double fix

--- controllers/PlantController.py
+++ controllers/PlantController.py
@@ -1,6 +1,9 @@
 from main import db
 from models.Plant import Plant
 from models.User import User
+import atexit
+from flask_mail import Mail, Message
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
 def add_plant_to_account(plant_id, u_name):
@@ -8,3 +11,40 @@ def add_plant_to_account(plant_id, u_name):
     plant = Plant.query.filter_by(id=plant_id).first()
     plant.ownership = user.id
     db.session.commit()
+
+
+def send_mail(message, subject, recipient):
+    with app.app_context():
+        mail = Mail(app)
+        msg = Message(body=message,
+                      subject=subject,
+                      sender="mucholowka@mail.com",
+                      recipients=[recipient])
+        mail.send(msg)
+
+
+def choose_alert_kind(form):
+    if form == "Podlewanie":
+        return "Podlej roślinę"
+    if form == "Nawożenie":
+        return "Nawieź roślinę"
+
+
+def schedule_mail(mail_app, context, frequency):
+    global app
+    app = mail_app
+    scheduler = BackgroundScheduler()
+    if frequency == "Codziennie":
+        scheduler.add_job(func=send_mail, kwargs=context, trigger="interval", days=1)
+    if frequency == "Tygodniowo":
+        scheduler.add_job(func=send_mail, kwargs=context, trigger="interval", days=7)
+    if frequency == "Miesięcznie":
+        scheduler.add_job(func=send_mail, kwargs=context, trigger="interval", months=1)
+    if frequency == "Kwartalnie":
+        scheduler.add_job(func=send_mail, kwargs=context, trigger="interval", months=3)
+    if frequency == "Rocznie":
+        scheduler.add_job(func=send_mail, kwargs=context, trigger="interval", years=1)
+    scheduler.start()
+
+    # Shut down the scheduler when exiting the app
+    atexit.register(lambda: scheduler.shutdown())
--- routes/PlantRouting.py
+++ routes/PlantRouting.py
@@ -79,3 +79,18 @@ def remove(plant_id):
                 plant.ownership = None
                 db.session.commit()
                 return redirect(url_for("collection"))
+
+
+@app.route("/alert", methods=['GET', 'POST'])
+def alert():
+    if session.get("username") is not None:
+        if request.method == 'GET':
+            return render_template('addNotification.html', plants=User.query.filter_by(username=session.
+                                                                                       get('username')).first().plants)
+        else:
+            email = User.query.filter_by(username=session.get('username')).first().email
+            context = {"recipient": email, "subject": "Przypomnienie z aplikacji Muchołówka",
+                       "message": session.get("username") + " " + choose_alert_kind(request.form['kind']) + " " +
+                                  request.form['plant']}
+            schedule_mail(app, context, request.form['frequency'])
+            return redirect(url_for("start"))

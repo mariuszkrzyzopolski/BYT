781c0c2a34513f0cffdb2d80466262b3883ba026
warhir <krzyzopolski.mariusz@gmail.com>
2022-01-06 17:22:21 +0100

    fix photo and list of plants

--- routes/PlantRouting.py
+++ routes/PlantRouting.py
@@ -85,7 +85,8 @@ def remove(plant_id):
 def alert():
     if session.get("username") is not None:
         if request.method == 'GET':
-            return render_template('addNotification.html')
+            return render_template('addNotification.html', plants=User.query.filter_by(username=session.
+                                                                                       get('username')).first().plants)
         else:
             email = User.query.filter_by(username=session.get('username')).first().email
             context = {"recipient": email, "subject": "Przypomnienie z aplikacji Muchołówka",
